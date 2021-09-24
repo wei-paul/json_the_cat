e5077ad16fbbd7f253c4d838f572916ea8dea93f
wei-paul <paul.wei607@gmail.com>
2021-09-24 03:07:38 +0000

    added mocha/chai testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
-fs = require('fs'); //require the built-in library "file system"
-let searchTerm = process.argv[2];
-const noop = () => {};
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`, (error, response, body) => {
-  if (error) {
-    return console.log("Sorry! Something happened with the request!");
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (!breed) {
-    return console.log('Sorry, breed not found!');
-  }
-  console.log(breed.description);
-  console.log(typeof data);
-});
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+     return callback(Error, null);
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (!breed) {
+     return callback(Error, null);
+    }
+    callback(null, breed.description);
+  });
+}
+
+module.exports = { fetchBreedDescription };
